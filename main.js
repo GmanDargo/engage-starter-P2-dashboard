bc84ab0c44e2fcea66cc4099b5898f0d49fc22d3
GmanDargo <mgburski@gmail.com>
2016-05-20 15:41:27 -0600

    first night

--- main.js
+++ main.js
@@ -1,23 +1,64 @@
 
+var tableElement = document.getElementById('dashboard-list');
+var personsAreaElement = document.getElementById('persons-area');
 
-var tableElement = document.getElementById('dashboard-list');  //finds element in html file with this id - dashboard-list
+var persons = [
+    {name: 'Fire Fly', info: 'I like my ship', img: 'just-keep-flying-detail-9281.jpg'},
+    {name: 'Moss', info: 'There\'s a fire Roy', img: 'imgres.jpg'}
+]
 
 var tableList = [
     {rank: '1', name: 'LoL', type: 'MOBA', cost: 'free',  year: '2010'},
-    {rank: '2', name: 'Half-life', type: 'FPS', cost: '$19',  year: '2005'},
-    {rank: '3', name: 'Starcraft', type: 'RTS', cost: '$19',  year: '2008'}
+    {rank: '2', name: 'Half-Life', type: 'FPS', cost: '$19',  year: '2005'},
+    {rank: '3', name: 'StarCraft', type: 'RTS', cost: '$19',  year: '2008'}
 ];
 
-for (var i=0; i < tableList.length; i++) {
-    var tr = document.createElement('TR'); 
-    for (var key in tableList[i]) {
-        if (tableList[i].hasOwnProperty(key)) {
-            var element = tableList[i][key];
-            var td = document.createElement('td')
-            var tdText = document.createTextNode(element);
-            td.appendChild(tdText);
-            tr.appendChild(td);
+dashboard = {};
+
+dashboard.buildTable = function buildTable(value) {
+    if (value) {value = value.toLowerCase()}
+    tableElement.innerHTML = '';
+    for (var i=0; i < tableList.length; i++) {
+        var tr = document.createElement('TR');
+        var contentExists = false;
+        for (var key in tableList[i]) {
+            if (tableList[i].hasOwnProperty(key)) {
+                var element = tableList[i][key];
+                var td = document.createElement('td');
+                if (element.toLowerCase().indexOf(value) > -1) {
+                    contentExists = true;
+                }
+                var tdText = document.createTextNode(element);
+                td.appendChild(tdText);
+                tr.appendChild(td);
+            }
         }
-    }
-    tableElement.appendChild(tr);
+        if (contentExists) tableElement.appendChild(tr);
+    }   
 }
+
+
+dashboard.buildPersonsArea = function buildPersonsArea(value) {
+    personsAreaElement.innerHTML = '';
+    for (var i = 0; i < persons.length; i++) {
+        var element = persons[i];
+        if (element.name.toLowerCase().indexOf(value.toLowerCase())) { 
+            personsAreaElement.innerHTML += 
+                '<div class="col-xs-6 col-sm-3 placeholder">'+
+                    '<img src="'+element.img+'" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">' +
+                    '<h4>'+element.name+'</h4>' +
+                    '<span class="text-muted">'+element.info+'</span>' +
+                '</div>'
+        }           
+    }    
+}
+
+dashboard.search = function search(value) {
+    // if (value.length > 2) {
+    dashboard.buildTable(value);  
+    dashboard.buildPersonsArea(value) 
+    // }
+}
+
+dashboard.buildPersonsArea('');
+dashboard.buildTable('');
